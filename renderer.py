273b24734c839c714a59f9eee2a9d2a5da47c6bb
elidor22 <elidorv@outlook.com>
2022-04-10 18:49:22 +0200

    Improved render script, enbled gpu support, used object oriented principles

--- renderer.py
+++ renderer.py
@@ -6,65 +6,70 @@ from math import *
 from mathutils import *
 import os
 
+class renderer:
 
-def enable_gpus(device_type, use_cpus=False):
-    preferences = bpy.context.preferences
-    cycles_preferences = preferences.addons["cycles"].preferences
-    cycles_preferences.refresh_devices()
-    devices = cycles_preferences.devices
+    def enable_gpus(device_type, use_cpus=False):
+        preferences = bpy.context.preferences
+        cycles_preferences = preferences.addons["cycles"].preferences
+        cycles_preferences.refresh_devices()
+        devices = cycles_preferences.devices
 
-    if not devices:
-        raise RuntimeError("Unsupported device type")
+        if not devices:
+            raise RuntimeError("Unsupported device type")
 
-    activated_gpus = []
-    for device in devices:
-        if device.type == "CPU":
-            device.use = use_cpus
-        else:
-            device.use = True
-            activated_gpus.append(device.name)
-            print('activated gpu', device.name)
+        activated_gpus = []
+        for device in devices:
+            if device.type == "CPU":
+                device.use = use_cpus
+            else:
+                device.use = True
+                activated_gpus.append(device.name)
+                print('activated gpu', device.name)
 
-    cycles_preferences.compute_device_type = device_type
-    bpy.context.scene.cycles.device = "GPU"
+        cycles_preferences.compute_device_type = device_type
+        bpy.context.scene.cycles.device = "GPU"
 
-    return activated_gpus
-
-
-# enable_gpus("CUDA")
+        return activated_gpus
 
 
 
+    def render():
+        # Import obj from the path given
+        bpy.ops.import_scene.obj(filepath=sys.argv[-1])
+
+        # Remove cube
+
+        objs = bpy.data.objects
+        objs.remove(objs["Cube"], do_unlink=True)
 
 
-# Import obj from the path given
-bpy.ops.import_scene.obj(filepath=sys.argv[-1])
+        #set your own target here
+        target = bpy.data.objects['model_normalized']
+        cam = bpy.data.objects['Camera']
+        t_loc_x = target.location.x
+        t_loc_y = target.location.y
+        cam_loc_x = cam.location.x
+        cam_loc_y = cam.location.y
 
-# Remove cube
+        #dist = sqrt((t_loc_x-cam_loc_x)**2+(t_loc_y-cam_loc_y)**2)
+        dist = (target.location.xy-cam.location.xy).length
+        #ugly fix to get the initial angle right
+        init_angle  = (1-2*bool((cam_loc_y-t_loc_y)<0))*acos((cam_loc_x-t_loc_x)/dist)-2*pi*bool((cam_loc_y-t_loc_y)<0)
 
-objs = bpy.data.objects
-objs.remove(objs["Cube"], do_unlink=True)
+        num_steps = 8 #how many rotation steps
+        for x in range(num_steps):
+            alpha = init_angle + (x+1)*2*pi/num_steps
+            cam.rotation_euler[2] = pi/2+alpha
+            cam.location.x = t_loc_x+cos(alpha)*dist
+            cam.location.y = t_loc_y+sin(alpha)*dist
+            file = f'/home/elidor/Videos/renders/{x}'
+            bpy.context.scene.render.filepath = file
+            bpy.ops.render.render( write_still=True ) 
 
 
-#set your own target here
-target = bpy.data.objects['model_normalized']
-cam = bpy.data.objects['Camera']
-t_loc_x = target.location.x
-t_loc_y = target.location.y
-cam_loc_x = cam.location.x
-cam_loc_y = cam.location.y
 
-#dist = sqrt((t_loc_x-cam_loc_x)**2+(t_loc_y-cam_loc_y)**2)
-dist = (target.location.xy-cam.location.xy).length
-#ugly fix to get the initial angle right
-init_angle  = (1-2*bool((cam_loc_y-t_loc_y)<0))*acos((cam_loc_x-t_loc_x)/dist)-2*pi*bool((cam_loc_y-t_loc_y)<0)
+# render = renderer
 
-num_steps = 8 #how many rotation steps
-for x in range(num_steps):
-    alpha = init_angle + (x+1)*2*pi/num_steps
-    cam.rotation_euler[2] = pi/2+alpha
-    cam.location.x = t_loc_x+cos(alpha)*dist
-    cam.location.y = t_loc_y+sin(alpha)*dist
-    file = f'/home/elidor/Videos/renders/{x}'
-    bpy.context.scene.render.filepath = file
-    bpy.ops.render.render( write_still=True ) 
+# # Enable CUDA accelerated rendering
+# render.enable_gpus('CUDA')
+# render.render()
