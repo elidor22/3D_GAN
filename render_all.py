eb43223517ad3673987446353b0da461ae0cd212
elidor22 <elidorv@outlook.com>
2022-04-11 12:48:07 +0200

    Made small improvements

--- render_all.py
+++ render_all.py
@@ -1,6 +1,6 @@
 import os, shutil
 from os import walk
-
+import tqdm
 # Get all folders in the given path
 path = '/media/elidor/CC98A71E98A70654/Ubuntu/3D_Data/02691156'
 # d = []
@@ -16,6 +16,7 @@ print(get_dirFolders(path))
 
 
 # Check if the render dir exists, create it, render images
+# remove directory with read-only files
 
 def render_onDirectory(path = '', ls = []):
     final_ls = []
@@ -23,18 +24,19 @@ def render_onDirectory(path = '', ls = []):
         pth = path + '/'+str(dir)
         final_ls.append(pth)
 
+
     for dir in final_ls:
         pth1 = dir+'/renders'
-        if  os.path.isdir(pth1):
-            shutil.rmtree(pth1)
+        if  not os.path.isdir(pth1):
+            print('Creating path...')
             os.mkdir(pth1)
-        else:
-            os.mkdir(pth1)
-        run_path = 'blender -b --python renderer.py -- '+ str(pth1)+' '+str(dir)+'/models/model_normalized.obj'
+            
+        run_path = 'blender -b --python renderer.py -- '+ str(pth1)+' '+str(dir)+'/models/model_normalized.obj' # Last element makes the render quiet on terminal
             # os.system(f'blender -b --python render.py -- {pth}/models/model_normalized.obj')
         os.system(run_path)
 
 
 path = '/media/elidor/CC98A71E98A70654/Ubuntu/3D_Data/test' 
+# path = '/media/elidor/CC98A71E98A70654/Ubuntu/3D_Data/02691156'
 directories = get_dirFolders(path)
 render_onDirectory(path=path, ls = directories)
